a6a1aaf1f6bcfa22c727d78e75e26018979faea2
david1wong1 <david1wong1@gmail.com>
2023-03-14 19:31:16 -0400

    Changed

---
+++ force-app/main/default/lwc/animeDatabase/animeDatabase.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+import WatchlistComponent from 'c/watchlistComponent';
+export default class AnimeDatabase extends LightningElement {}
---
+++ force-app/main/default/lwc/watchlistComponent/watchlistComponent.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class WatchlistComponent extends LightningElement {
+@api Watchlist;
+}
