f5bd2df0ca4dc4ca75dfb3df9ab31c8896d77309
david1wong1 <david1wong1@gmail.com>
2023-03-30 10:05:29 -0400

    Finished almost everything

--- force-app/main/default/lwc/randomAnimeComponent/randomAnimeComponent.js
+++ force-app/main/default/lwc/randomAnimeComponent/randomAnimeComponent.js
@@ -1,8 +1,8 @@
 import { LightningElement } from 'lwc';
-import randomAnimes from '@salesforce/apex/RandomAnimesController.randomAnimes';
+import getRandomAnimes from '@salesforce/apex/GetRandomAnimesController.getRandomAnimes';
 
 export default class RandomAnimeComponent extends LightningElement {
-    randomAnimesMap;
+    randomAnimes;
     anime1;
     anime2;
     anime3;
@@ -10,20 +10,19 @@ export default class RandomAnimeComponent extends LightningElement {
     anime5;
 
     connectedCallback() {
-        randomAnimes()
+        getRandomAnimes()
         .then(result => {
-            this.randomAnimesMap = result;
-            console.log(this.randomAnimesMap);
-            for (let key in this.randomAnimesMap) {
-                this.randomAnimesMap[key].Preview__c = this.randomAnimesMap[key].Preview__c.split('"',2)[1];
-                this.randomAnimesMap[key].pageURL = "https://salescraft-a-dev-ed.develop.lightning.force.com/lightning/r/Anime__c/" + this.randomAnimesMap[key].Id + "/view";
+            this.randomAnimes = result;
+            console.log(this.randomAnimes[0]);
+            for (let key in this.randomAnimes) {
+                this.randomAnimes[key].Preview__c = this.randomAnimes[key].Preview__c.split('"',2)[1];
+                this.randomAnimes[key].pageURL = "https://salescraft-a-dev-ed.develop.lightning.force.com/lightning/r/Anime__c/" + this.randomAnimes[key].Id + "/view";
             }
-            console.log(this.randomAnimesMap);
-            this.anime1 = this.randomAnimesMap[1];
-            this.anime2 = this.randomAnimesMap[2];
-            this.anime3 = this.randomAnimesMap[3];
-            this.anime4 = this.randomAnimesMap[4];
-            this.anime5 = this.randomAnimesMap[5];
+            this.anime1 = this.randomAnimes[0];
+            this.anime2 = this.randomAnimes[1];
+            this.anime3 = this.randomAnimes[2];
+            this.anime4 = this.randomAnimes[3];
+            this.anime5 = this.randomAnimes[4];
         });
     }
 
