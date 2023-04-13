a3b01fc5451dab6dcffa5944c8325c51f1a1ffd0
david1wong1 <david1wong1@gmail.com>
2023-04-13 16:41:22 -0400

    finished

--- force-app/main/default/lwc/dP2JSONPayloadHandler/dP2JSONPayloadHandler.js
+++ force-app/main/default/lwc/dP2JSONPayloadHandler/dP2JSONPayloadHandler.js
@@ -1,13 +1,44 @@
 import { LightningElement } from 'lwc';
-/*import insertContactInfo from '@salesforce/apex/DP2Callout.insertContactInfo';*/
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import insertContactInfo from '@salesforce/apex/DP2Callout.insertContactInfo';
 
 export default class DP2JSONPayloadHandler extends LightningElement {
     jsonText;
+    deserializedText;
 
     handleJSONChange(event) {
         this.jsonText = event.detail.value;
     }
     handleSendButtonClick() {
-        insertContactInfo({jsonStr : this.jsonText});
+        this.deserializedText = this.convertToPlain(this.jsonText);
+        console.log(this.deserializedText);
+        insertContactInfo({jsonStr : this.deserializedText
+        }).then(result => {
+           this.showToast();
+        }).catch(error => {
+            console.log('error');
+        });
+    };
+
+    showToast() {
+        var toastEvent = new ShowToastEvent(
+          {
+            title: 'Sent',
+            message:'Your Contact has been successfully inserted.',
+            variant: 'success',
+          }
+        );
+        this.dispatchEvent(toastEvent);
     }
+    convertToPlain(html){
+        // Create a new div element
+        var tempDivElement = document.createElement("div");
+    
+        // Set the HTML content with the given value
+        tempDivElement.innerHTML = html;
+    
+        // Retrieve the text property of the element 
+        return tempDivElement.textContent || tempDivElement.innerText || "";
+    }
+    
 }
